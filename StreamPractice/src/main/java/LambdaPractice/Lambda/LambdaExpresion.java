6cee9c322730a90546f3f01988c19eb78c507f08
Aware man <110034401+JayeshThakare98@users.noreply.github.com>
2024-11-06 21:44:34 +0530

    stream prac

--- StreamPractice/src/main/java/LambdaPractice/Lambda/LambdaExpresion.java
+++ StreamPractice/src/main/java/LambdaPractice/Lambda/LambdaExpresion.java
@@ -7,8 +7,8 @@ public class LambdaExpresion {
         myInter.sayHello();
 
 //        SumInter sumInter = (int a ,int b ) -> {return  a+b;};
-        SumInter sumInter = (a,b) ->   a+b;
-        System.out.println(sumInter.sum(10 , 10));
+        SumInter sumInter = (a, b) -> a + b;
+        System.out.println(sumInter.sum(10, 10));
 
         Lengthinterf lengthinterf = (str) -> str.length();
         System.out.println(lengthinterf.getLengthOfString("abcd"));
